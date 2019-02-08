d42562b88dd95422d2d9fa7cda2b4113a148b150
kiatAWDSA <lskiat@huskers.unl.edu>
2019-02-08 09:18:34 -0600

    Add project files.

---
+++ SHT3x.h
@@ -0,0 +1,14 @@
+// SHT3x.h
+
+#ifndef _SHT3X_h
+#define _SHT3X_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
---
+++ SHT3x.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		SHT3x.ino
+ Created:	2/8/2019 9:15:33 AM
+ Author:	slau4
+*/
+
+// the setup function runs once when you press reset or power the board
+#include "SHT3x.h"
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
